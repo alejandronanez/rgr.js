a6be0c002a6c6602ae3a40188f85213f86c2bf83
Alejandro Nanez <alejonanez@gmail.com>
2016-07-30 00:12:00 -0500

    Connected to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+require('dotenv').config();
 
-let app = express();
+const app = express();
 
 app.use(express.static('public'));
 
 app.listen(3000);
+
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+	if (err) {
+		throw err;
+	}
+
+	database.collection('links').find({}).toArray((e, links) => {
+		if (e) {
+			throw e;
+		}
+
+		console.log(links);
+	});
+});
